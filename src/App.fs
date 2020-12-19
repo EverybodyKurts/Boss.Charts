c93876b70ec2226d02975d3e57dc956af64c90bc
Kurt Mueller <kurtronaldmueller@gmail.com>
2020-12-19 15:13:33 -0500

    Format via fantomas

--- src/App.fs
+++ src/App.fs
@@ -4,8 +4,10 @@ open Feliz
 
 open Color
 
-let color = Color.create (Red 0) (Green 255) (Blue 255)
+let color =
+    Color.create (Red 0) (Green 255) (Blue 255)
+
 let colorString = Color.toRGBAstring color
 
 [<ReactComponent>]
-let HelloWorld() = Html.h1 $"Hello world {colorString}"
+let HelloWorld () = Html.h1 $"Hello world {colorString}"
--- src/Color.fs
+++ src/Color.fs
@@ -6,23 +6,19 @@ type Red = Red of int
 type Green = Green of int
 type Blue = Blue of int
 
-let clamp num =
-    Math.Clamp (num, 0, 255)
+let clamp num = Math.Clamp(num, 0, 255)
 
-type Color =
-    { R: Red
-      G: Green
-      B: Blue }
+type Color = { R: Red; G: Green; B: Blue }
 
 let create (Red r) (Green g) (Blue b) =
-    { R = Red (clamp r)
-      G = Green (clamp g)
-      B = Blue (clamp b) }
+    { R = Red(clamp r)
+      G = Green(clamp g)
+      B = Blue(clamp b) }
 
 let toRGBAstring color =
-    let { R = (Red r); G = (Green g); B = (Blue b) } = color
+    let { R = (Red r)
+          G = (Green g)
+          B = (Blue b) } =
+        color
 
     $"rgba({r}, {g}, {b}, 1)"
-
-
-
