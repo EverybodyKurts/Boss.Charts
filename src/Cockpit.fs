af144d0fbd12280c44addfc8e9c969aeb74ab7bd
Kurt Mueller <kurtronaldmueller@gmail.com>
2020-12-19 15:27:44 -0500

    Add cockpit colors

---
+++ src/Cockpit.fs
@@ -0,0 +1,13 @@
+module Cockpit
+
+open Color
+
+let colors =
+    {| Red = Color.create (Red 255) (Green 0) (Blue 0)
+       Yellow = Color.create (Red 255) (Green 255) (Blue 0)
+       Green = Color.create (Red 0) (Green 255) (Blue 0)
+       Black = Color.create (Red 0) (Green 0) (Blue 0)
+       DarkGreen = Color.create (Red 40) (Green 167) (Blue 69)
+       Mustard = Color.create (Red 255) (Green 193) (Blue 7)
+       Blue = Color.create (Red 23) (Green 162) (Blue 1)
+    |}
